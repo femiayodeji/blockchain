18891638b1ff80e17ce55dc5b95d3a5f5e233a29
femi <femidotayodeji@gmail.com>
2021-02-07 22:25:25 +0100

    Added data security test

--- BlockchainSeed.cs
+++ BlockchainSeed.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Diagnostics;
 using Newtonsoft.Json;
 
 public static class BlockchainTest
 {
     public static void Run()
+    {
+        Seed();
+        DataSecurity();
+    }
+
+    public static void Seed()
     {
         Chain testCoin = new Chain();  
         testCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Femi,receiver:Adetayo,amount:10}"));  
@@ -11,6 +18,20 @@ public static class BlockchainTest
         testCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Adetayo,receiver:Femi,amount:5}"));  
         
         Console.WriteLine(JsonConvert.SerializeObject(testCoin, Formatting.Indented));
+    }
+
+    public static void DataSecurity()
+    {
+        Chain testCoin = new Chain();  
+        testCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Femi,receiver:Adetayo,amount:10}"));  
+        testCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Adetayo,receiver:Femi,amount:5}"));  
+        testCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Adetayo,receiver:Femi,amount:5}"));  
+        
+        Debug.Assert(testCoin.IsChainValid() == true, "Data Security Test Failed", "The chain is valid. Expected true");
+
+        testCoin.DataBase[1].Data = $"";
+
+        Debug.Assert(testCoin.IsChainValid() == true, "Data Security Test Failed", "The chain is invalid. Expected false");
     }
 }
 
